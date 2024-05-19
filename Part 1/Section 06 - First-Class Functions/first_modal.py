710186e307b5eb4b9c88414246c7e29ba65ad943
Andres Montenegro <26306411+andresckamilo@users.noreply.github.com>
2024-05-19 12:29:10 +0200

    Study Python

---
+++ Part 1/Section 06 - First-Class Functions/first_modal.py
@@ -0,0 +1,17 @@
+import modal
+
+app = modal.App(
+    "example get started"
+
+
+)
+
+
+@app.function()
+def square(x):
+    print("This code is running on a remote worker!")
+    return x**2
+
+@app.local_entrypoint()
+def main():
+    print(f"The square is {square.remote(42)=}")
